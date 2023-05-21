84342296516f629d5e532f39c58f85cd90234c71
ShubhamKhaire666 <shubhamkhaire.edu@gmail.com>
2023-05-21 23:41:10 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DollarStoreAmazon.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
