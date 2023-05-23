2db6fe058e7cfb05d070fc9ce506905f3d42d761
ShubhamKhaire666 <shubhamkhaire.edu@gmail.com>
2023-05-23 22:45:24 +0300

    Migration for Sql Database

---
+++ Data/DollarStoreAmazonDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DollarStoreAmazon.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DollarStoreAmazon.Data
+{
+    public class DollarStoreAmazonDbContext : DbContext
+    {
+        public DollarStoreAmazonDbContext(DbContextOptions<DollarStoreAmazonDbContext> options) : base(options)
+        {
+        
+        }
+
+        public DbSet<Category> Categories{ get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using DollarStoreAmazon.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DollarStoreAmazonDbContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
